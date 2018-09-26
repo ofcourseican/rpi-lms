24378c8b40c2546f5d1256499acc20f9a69b60a7
Kai Timmer <kai@staffbase.com>
2018-09-26 11:00:41 +0200

    Revert ":bug: setting access rights in entrypoint"
    This reverts commit f3e10ac431f6fb90351a40faafde8173c8197db8.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-chown -R squeezeboxserver: /var/lib/squeezeboxserver
-chown -R squeezeboxserver: /var/log/squeezeboxserver
-
 exec squeezeboxserver --user squeezeboxserver \
 	--prefsdir $SQUEEZE_VOL_PERSIST/prefs \
 	--logdir $SQUEEZE_VOL_LOG \
