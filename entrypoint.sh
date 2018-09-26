f3e10ac431f6fb90351a40faafde8173c8197db8
Kai Timmer <kai@staffbase.com>
2018-09-26 10:53:01 +0200

    :bug: setting access rights in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+chown -R squeezeboxserver: /var/lib/squeezeboxserver
+chown -R squeezeboxserver: /var/log/squeezeboxserver
+
 exec squeezeboxserver --user squeezeboxserver \
 	--prefsdir $SQUEEZE_VOL_PERSIST/prefs \
 	--logdir $SQUEEZE_VOL_LOG \
